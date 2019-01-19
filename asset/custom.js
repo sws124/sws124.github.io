c589cc66f9f56318764e9d7a8c1ca5f9bbc91c89
sws124 <suenws0124@gmail.com>
2019-01-20 02:50:38 +0800

    preset mode

--- asset/custom.js
+++ asset/custom.js
@@ -1,33 +1,5 @@
-var timer, time, score, opList, op, num1, num2, ans;
-
-function getBound(b) {
-    return Number($("#"+b+"-bound").val());
-}
-
-function setBound(b, i) {
-    $("#"+b+"-bound").val(Number(i));
-}
-
-function validate() {
-    if (!$("#allow-negative").hasClass("active")) {
-        if (getBound("lower") < 0) {
-            setBound("lower", Math.abs(getBound("lower")));
-        }
-        if (getBound("upper") < 0) {
-            setBound("upper", Math.abs(getBound("upper")));
-        }
-    }
-    if (!$("#allow-floating").hasClass("active")) {
-        setBound("lower", Math.floor(getBound("lower")));
-        setBound("upper", Math.floor(getBound("upper")));
-    }
-    if (getBound("lower") > getBound("upper")) {
-        var tmp = getBound("lower");
-        setBound("lower", getBound("upper"));
-        setBound("upper", tmp);
-    }
-    $("#time-limit").val(Math.abs(Math.floor(Number($("#time-limit").val()))));
-}
+var timer, mode, setting = [[120, 2, 9, 1, 1, 1, 0, 2, 4], [120, 10, 99, 1, 1, 0, 0, 4, 3], [120, 2, 99, 0, 0, 1, 1, 6, 2]];
+var opList, op, time, score, num1, num2, ans;
 
 function countdown() {
     if (time > 0) {
@@ -36,100 +8,136 @@ function countdown() {
     } else {
         clearInterval(timer);
         $(".question").hide();
-        $("#message").hide();
-        $(".option-button").attr("disabled", false);
-        $("#lower-bound").attr("disabled", false);
-        $("#upper-bound").attr("disabled", false);
-        $("#time-limit").attr("disabled", false);
+        $(".option").show();
         $("#btn-start").show();
-        $("#message-pokemon").show();
-        if (score < 5) {
+        var interval = 10;
+        if (score <= interval * 1) {
+            $("#message").text("你得到一隻爛泥怪！");
             $("#pokemon").attr("src", "asset/img/Q-88.png");
-        } else if (score < 10) {
+        } else if (score <= interval * 2) {
+            $("#message").text("你得到一隻小拳石！");
             $("#pokemon").attr("src", "asset/img/P-74.png");
-        } else if (score < 15) {
+        } else if (score <= interval * 3) {
+            $("#message").text("你得到一隻鐵甲犀牛！");
             $("#pokemon").attr("src", "asset/img/O-111.png");
-        } else if (score < 20) {
+        } else if (score <= interval * 4) {
+            $("#message").text("你得到一隻大顎蟻！");
             $("#pokemon").attr("src", "asset/img/N-328.png");
-        } else if (score < 25) {
+        } else if (score <= interval * 5) {
+            $("#message").text("你得到一隻巴爾郎！");
             $("#pokemon").attr("src", "asset/img/M-236.png");
-        } else if (score < 30) {
+        } else if (score <= interval * 6) {
+            $("#message").text("你得到一隻大岩蛇！");
             $("#pokemon").attr("src", "asset/img/L-95.png");
-        } else if (score < 35) {
+        } else if (score <= interval * 7) {
+            $("#message").text("你得到一隻飛天螳螂！");
             $("#pokemon").attr("src", "asset/img/K-123.png");
-        } else if (score < 40) {
+        } else if (score <= interval * 8) {
+            $("#message").text("你得到一隻狃拉！");
             $("#pokemon").attr("src", "asset/img/J-215.png");
-        } else if (score < 45) {
+        } else if (score <= interval * 9) {
+            $("#message").text("你得到一隻行路草！");
             $("#pokemon").attr("src", "asset/img/I-43.png");
-        } else if (score < 50) {
+        } else if (score <= interval * 10) {
+            $("#message").text("你得到一隻拉魯拉絲！");
             $("#pokemon").attr("src", "asset/img/H-280.png");
-        } else if (score < 55) {
+        } else if (score <= interval * 11) {
+            $("#message").text("你得到一隻雪童子！");
             $("#pokemon").attr("src", "asset/img/G-361.png");
-        } else if (score < 60) {
+        } else if (score <= interval * 12) {
+            $("#message").text("你得到一隻夢妖！");
             $("#pokemon").attr("src", "asset/img/F-200.png");
-        } else if (score < 65) {
+        } else if (score <= interval * 13) {
+            $("#message").text("你得到一隻三蜜蜂！");
             $("#pokemon").attr("src", "asset/img/E-415.png");
-        } else if (score < 70) {
+        } else if (score <= interval * 14) {
+            $("#message").text("你得到一隻帕奇利茲！");
             $("#pokemon").attr("src", "asset/img/D-417.png");
-        } else if (score < 75) {
+        } else if (score <= interval * 15) {
+            $("#message").text("你得到一隻笨笨魚！");
             $("#pokemon").attr("src", "asset/img/C-349.png");
-        } else if (score < 80) {
+        } else if (score <= interval * 16) {
+            $("#message").text("你得到一隻小火馬！");
             $("#pokemon").attr("src", "asset/img/B-77.png");
         } else {
+            $("#message").text("你得到一隻伊貝！");
             $("#pokemon").attr("src", "asset/img/A-133.png");
         }
+        $("#message-pokemon").show();
     }
 }
 
 function ask() {
-    var valid;
-    do {
-        valid = true;
-        op = opList[Math.floor(Math.random() * opList.length)];
-        num1 = Math.floor(Math.random() * (getBound("upper") - getBound("lower") + 1) + getBound("lower"));
-        num2 = Math.floor(Math.random() * (getBound("upper") - getBound("lower") + 1) + getBound("lower"));
-        switch (op) {
-            case "+": ans = num1 + num2; break;
-            case "-": ans = num1 - num2; break;
-            case "×": ans = num1 * num2; break;
-            case "÷": ans = num1 / num2; break;
-        }
-        if (ans < 0 && !$("#allow-negative").hasClass("active")) {
-            valid = false;
-        }
-        if (ans != Math.floor(ans) && !$("#allow-floating").hasClass("active")) {
-            valid = false;
-        }
-    } while (!valid);
+    op = opList[Math.floor(Math.random() * opList.length)];
+    num1 = Math.floor(Math.random() * (setting[mode][2] - setting[mode][1] + 1) + setting[mode][1]);
+    num2 = Math.floor(Math.random() * (setting[mode][2] - setting[mode][1] + 1) + setting[mode][1]);
+    switch (op) {
+        case "+": ans = num1 + num2; break;
+        case "-":
+            ans = num1 - num2;
+            if (ans < 0) {
+                num2 = num1 + num2;
+                num1 = num2 - num1;
+                num2 = num2 - num1;
+            }
+            break;
+        case "×":
+            if (num1 > 11 && num2 > 11) {
+                if (Math.random() < 0.5) {
+                    num1 = num1 % 10;
+                    num1 = (num1 >= setting[mode][1] ? num1 : setting[mode][1]);
+                } else {
+                    num2 = num2 % 10;
+                    num2 = (num2 >= setting[mode][1] ? num2 : setting[mode][1]);
+                }
+            }
+            ans = num1 * num2;
+            break;
+        case "÷":
+            if (num2 > 11) {
+                num2 = num2 % 10;
+                num2 = (num2 >= setting[mode][1] ? num2 : setting[mode][1]);
+            }
+            ans = num1 * num2;
+            ans = num1 + ans;
+            num1 = ans - num1;
+            ans = ans - num1;
+            break;
+    }
     $("#equation").text(num1 + " " + op + " " + num2 + " = ");
 }
 
 $(document).ready(function() {
     $(".game-panel").hide();
     $(".option-button").click(function() {
-        if ($(this).hasClass("active")) {
-            $(this).removeClass("active");
-        } else {
-            $(this).addClass("active");
+        $(".option-button").removeClass("active");
+        $(this).addClass("active");
+        mode = Number($(this).attr("id")[4]);
+        var tmp = "";
+        if (setting[mode][3]) {
+            tmp += "加數、";
         }
+        if (setting[mode][4]) {
+            tmp += "減數、";
+        }
+        if (setting[mode][5]) {
+            tmp += "乘數、";
+        }
+        if (setting[mode][6]) {
+            tmp += "除數、";
+        }
+        tmp = tmp.slice(0, -1);
+        $("#description").html("限時"+setting[mode][0]+"秒，數字範圍由"+setting[mode][1]+"至"+setting[mode][2]+"，包含"+tmp+"。<br>答對得"+setting[mode][7]+"分，答錯扣"+setting[mode][8]+"分。");
     });
-    $("#lower-bound").change(function() {
-        validate();
-    });
-    $("#upper-bound").change(function() {
-        validate();
-    });
-    $("#time-limit").change(function() {
-        validate();
-    });
-    $("#answer").bind("enterKey", function(e){
+    $("#mode0").click();
+    $("#answer").bind("enterKey", function(e) {
         if (Number($("#answer").val()) == ans) {
-            score += 2;
+            score += setting[mode][7];
             $("#message").text("答對了："+$("#equation").text()+ans);
             $("#answer").val("");
             ask();
         } else {
-            score--;
+            score -= setting[mode][8];;
             $("#message").text("答錯了！");
             $("#answer").val("");
         }
@@ -141,29 +149,30 @@ $(document).ready(function() {
         }
     });
     $("#btn-start").click(function() {
-        validate();
         score = 0;
-        time = Number($("#time-limit").val()) * 100;
+        time = setting[mode][0] * 100;
         $("#score").text(score);
         $("#time").text((time/100).toFixed(2));
-        opList = ["+"];
-        if ($("#allow-subtraction").hasClass("active")) {
+        opList = [];
+        if (setting[mode][3]) {
+            opList.push("+");
+        }
+        if (setting[mode][4]) {
             opList.push("-");
         }
-        if ($("#allow-multiplication").hasClass("active")) {
+        if (setting[mode][5]) {
             opList.push("×");
         }
-        if ($("#allow-division").hasClass("active")) {
+        if (setting[mode][6]) {
             opList.push("÷");
         }
-        $(".option-button").attr("disabled", true);
-        $("#lower-bound").attr("disabled", true);
-        $("#upper-bound").attr("disabled", true);
-        $("#time-limit").attr("disabled", true);
+        $(".option").hide();
         $("#btn-start").hide();
+        $("#game-title").text("數學遊戲："+$("#mode"+mode).text());
+        $("#answer").val("");
         $(".game-panel").show();
         $(".question").show();
-        $("#message").show();
+        $("#message").text("遊戲開始");
         $("#message-pokemon").hide();
         $("#answer").focus();
         timer = setInterval(countdown, 10);
